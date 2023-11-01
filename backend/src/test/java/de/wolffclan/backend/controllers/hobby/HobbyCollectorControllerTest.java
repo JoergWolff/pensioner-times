3462bb07019e6f15b38880a805432ef8773bffe9
JoergWolff <neuerfisch@wolffclan.de>
2023-11-01 15:13:54 +0100

    add HobbyCollector backend tests fix

--- backend/src/test/java/de/wolffclan/backend/controllers/hobby/HobbyCollectorControllerTest.java
+++ backend/src/test/java/de/wolffclan/backend/controllers/hobby/HobbyCollectorControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -35,6 +36,7 @@ class HobbyCollectorControllerTest {
     }
 
     @Test
+    @DirtiesContext
     void getAllHobbyCollectors_ReturnsArrayOfUser() throws Exception {
 
         HobbyCollector hobbyCollector = new HobbyCollector("1", "Test", 1);
